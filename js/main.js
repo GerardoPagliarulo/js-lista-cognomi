f5cd64e344c6ca00c332ea2ce05cb8177c04e829
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-03-31 20:03:14 +0200

    Lista Cognomi

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@
 var cognomeUtente
 var listaCognomi
 
+// Reference For
+var elenco = document.getElementById('lista');
+
 // Chiedere all' utente il cognome
 cognomeUtente = prompt('Inserire il proprio cognome:');
-//Lista cognomi
+// Lista cognomi
 listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 
 // Inserire il nome digitato in un array con altri cognomi
@@ -17,8 +20,17 @@ listaCognomi.sort();
 
 for (i = 0; i < listaCognomi.length; i++) {
     nuovaLista = listaCognomi[i];
-    console.log(nuovaLista);
+    // console.log(nuovaLista);
+
+    var cognomePrecedente = elenco.innerHTML;
+    console.log(cognomePrecedente);
+
+    var nuovoCognome = '<li>' + nuovaLista + '</li>';
+    elenco.innerHTML = cognomePrecedente + nuovoCognome;
 }
 
 // Indicare la posizione occupata nella lista dal cognome del nuovo utente
-console.log('Il tuo nome è alla posizione numero: ' + (listaCognomi.indexOf(cognomeUtente) + 1));
+console.log('Il tuo cognome è alla posizione numero: ' + (listaCognomi.indexOf(cognomeUtente) + 1));
+
+// Mostare a Schermo
+document.getElementById('posizione-cognome').innerHTML = (listaCognomi.indexOf(cognomeUtente) + 1);
